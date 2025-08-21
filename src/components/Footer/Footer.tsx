b63ddb79aeb95fe4f37e07043a560fa5ba4d2047
ShivamRauniyaar <shivam@wearetechtonic.com>
2025-08-21 13:32:38 +0530

    fixing in footer and improving ui

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,29 +3,255 @@ import Link from "next/link";
 import Image from "next/image";
 import { SHAPES_CMS_URL } from "../../../Constant";
 
-async function getFooterData() {
-  try {
-    const res = await fetch(`${SHAPES_CMS_URL}Footer`, {
-      headers: {
-        "Content-Type": "application/json",
-        Origin: "https://shapesproduct.netlify.app/",
-      },
-      next: { revalidate: 86400 },
-    });
 
-    if (!res.ok) {
-      throw new Error("Failed to fetch data");
-    }
-
-    const data = await res.json();
-    return data?.data?.[0] || null;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
-}
 const Footer = async () => {
-  const footerDataValue = await getFooterData();
+  const footerDataValue = {
+    id: "a2ea4062-8fbd-4f5d-864e-6a088aabac38",
+    content: {
+      title: "Footer",
+      address: [
+        {
+          text: "Corporate Address",
+          content:
+            "A-96/11, Wazirpur Industrial Area, New Delhi - 110052, India",
+        },
+        {
+          text: "Factory Address",
+          content:
+            "Plot No. 17C, HSIIDC, Phase V Sector-53, Kundli - 131028 Sonipat, Haryana, India",
+        },
+        {
+          text: "Mail",
+          content:
+            ' <a href="mailto:info@shapesproducts.in">info@shapesproducts.in</a>',
+        },
+        {
+          text: "Phone",
+          content: ' <a href="tel:+919896111273">+91 9896111273</a>',
+        },
+      ],
+      navigation: [
+        {
+          heading: "Quick Links",
+          submenu: [
+            {
+              url: "/",
+              title: "Home",
+            },
+            {
+              url: "/our-story",
+              title: "Our Story",
+            },
+            {
+              url: "/our-team",
+              title: "Our Team",
+            },
+          ],
+        },
+        {
+          heading: "Important Links",
+          submenu: [
+            {
+              url: "/pages/refund-return-policy",
+              title: "Refund and Return Policy",
+            },
+            {
+              url: "/pages/refund-return-policy",
+              title: "Privacy and Policy",
+            },
+            {
+              url: "/pages/terms-condition",
+              title: "Terms and Condition",
+            },
+          ],
+        },
+        {
+          heading: "Get in Touch",
+          submenu: [
+            {
+              url: "/contact-us",
+              title: "Contact Us",
+            },
+          ],
+        },
+        {
+          heading: "Newletter",
+          submenu: [
+            {
+              url: "#",
+              title: "A SPOONFUL OF HAPPINESS",
+            },
+          ],
+        },
+      ],
+      socialmedia: [
+        {
+          id: "95b4a517-8f94-46d7-a181-4658c882847a",
+          url: "https://www.youtube.com/@ShapesProducts",
+          icons: [
+            {
+              src: "https://assets.cms.ecollat.com/cmsassets/null/92fecef5-9e67-448e-b436-6c91fc3af9fc/Z3OnyY6dIxHP/og_1741341476_icons8-youtube-logo.svg",
+              caption: null,
+              filePath:
+                "https://assets.cms.ecollat.com/cmsassets/null/92fecef5-9e67-448e-b436-6c91fc3af9fc/Z3OnyY6dIxHP/og_1741341476_icons8-youtube-logo.svg",
+              fileSize: "585 bytes",
+              dimension: null,
+              mediaType: "image",
+              mediaFileId: "Z3OnyY6dIxHP",
+              alternateName: null,
+              thumbnailPath:
+                "https://assets.cms.ecollat.com/cmsassets/null/92fecef5-9e67-448e-b436-6c91fc3af9fc/Z3OnyY6dIxHP/og_1741341476_icons8-youtube-logo.svg",
+              fileOriginalName: "icons8-youtube-logo.svg",
+              alternateFilePath: null,
+              alternateThumbnail: null,
+            },
+          ],
+          title: "Youtube",
+          whiteicon: [
+            {
+              src: "https://assets.cms.ecollat.com/cmsassets/null/92fecef5-9e67-448e-b436-6c91fc3af9fc/kkxGUrhCx-Z4/og_1741427071_icons8-youtube.svg",
+              caption: null,
+              filePath:
+                "https://assets.cms.ecollat.com/cmsassets/null/92fecef5-9e67-448e-b436-6c91fc3af9fc/kkxGUrhCx-Z4/og_1741427071_icons8-youtube.svg",
+              fileSize: "1.65 KB",
+              dimension: null,
+              mediaType: "image",
+              mediaFileId: "kkxGUrhCx-Z4",
+              alternateName: null,
+              thumbnailPath:
+                "https://assets.cms.ecollat.com/cmsassets/null/92fecef5-9e67-448e-b436-6c91fc3af9fc/kkxGUrhCx-Z4/og_1741427071_icons8-youtube.svg",
+              fileOriginalName: "icons8-youtube.svg",
+              alternateFilePath: null,
+              alternateThumbnail: null,
+            },
+          ],
+        },
+        {
+          id: "664bd14d-aff1-4ae9-a246-98ff961187b4",
+          url: "https://twitter.com/productsshape",
+          icons: [
+            {
+              src: "https://assets.cms.ecollat.com/cmsassets/null/92fecef5-9e67-448e-b436-6c91fc3af9fc/1OLNYD5wr6Nd/og_1741341472_icons8-twitter-bird.svg",
+              caption: null,
+              filePath:
+                "https://assets.cms.ecollat.com/cmsassets/null/92fecef5-9e67-448e-b436-6c91fc3af9fc/1OLNYD5wr6Nd/og_1741341472_icons8-twitter-bird.svg",
+              fileSize: "1.15 KB",
+              dimension: null,
+              mediaType: "image",
+              mediaFileId: "1OLNYD5wr6Nd",
+              alternateName: null,
+              thumbnailPath:
+                "https://assets.cms.ecollat.com/cmsassets/null/92fecef5-9e67-448e-b436-6c91fc3af9fc/1OLNYD5wr6Nd/og_1741341472_icons8-twitter-bird.svg",
+              fileOriginalName: "icons8-twitter-bird.svg",
+              alternateFilePath: null,
+              alternateThumbnail: null,
+            },
+          ],
+          title: "Twitter",
+          whiteicon: [
+            {
+              src: "https://assets.cms.ecollat.com/cmsassets/null/92fecef5-9e67-448e-b436-6c91fc3af9fc/vQH-AAKGHSC-/og_1741427102_icons8-twitter-bird1.svg",
+              caption: null,
+              filePath:
+                "https://assets.cms.ecollat.com/cmsassets/null/92fecef5-9e67-448e-b436-6c91fc3af9fc/vQH-AAKGHSC-/og_1741427102_icons8-twitter-bird1.svg",
+              fileSize: "1.60 KB",
+              dimension: null,
+              mediaType: "image",
+              mediaFileId: "vQH-AAKGHSC-",
+              alternateName: null,
+              thumbnailPath:
+                "https://assets.cms.ecollat.com/cmsassets/null/92fecef5-9e67-448e-b436-6c91fc3af9fc/vQH-AAKGHSC-/og_1741427102_icons8-twitter-bird1.svg",
+              fileOriginalName: "icons8-twitter-bird (1).svg",
+              alternateFilePath: null,
+              alternateThumbnail: null,
+            },
+          ],
+        },
+        {
+          id: "7a1cd668-f14d-49af-9ee2-730d031c6e74",
+          url: "https://www.instagram.com/shapesproducts/",
+          icons: [
+            {
+              src: "https://assets.cms.ecollat.com/cmsassets/null/92fecef5-9e67-448e-b436-6c91fc3af9fc/pEpBWujcnjdR/og_1741341465_icons8-instagram.svg",
+              caption: null,
+              filePath:
+                "https://assets.cms.ecollat.com/cmsassets/null/92fecef5-9e67-448e-b436-6c91fc3af9fc/pEpBWujcnjdR/og_1741341465_icons8-instagram.svg",
+              fileSize: "705 bytes",
+              dimension: null,
+              mediaType: "image",
+              mediaFileId: "pEpBWujcnjdR",
+              alternateName: null,
+              thumbnailPath:
+                "https://assets.cms.ecollat.com/cmsassets/null/92fecef5-9e67-448e-b436-6c91fc3af9fc/pEpBWujcnjdR/og_1741341465_icons8-instagram.svg",
+              fileOriginalName: "icons8-instagram.svg",
+              alternateFilePath: null,
+              alternateThumbnail: null,
+            },
+          ],
+          title: "instagram",
+          whiteicon: [
+            {
+              src: "https://assets.cms.ecollat.com/cmsassets/null/92fecef5-9e67-448e-b436-6c91fc3af9fc/-TRgaPFVEaga/og_1741427114_icons8-instagram1.svg",
+              caption: null,
+              filePath:
+                "https://assets.cms.ecollat.com/cmsassets/null/92fecef5-9e67-448e-b436-6c91fc3af9fc/-TRgaPFVEaga/og_1741427114_icons8-instagram1.svg",
+              fileSize: "915 bytes",
+              dimension: null,
+              mediaType: "image",
+              mediaFileId: "-TRgaPFVEaga",
+              alternateName: null,
+              thumbnailPath:
+                "https://assets.cms.ecollat.com/cmsassets/null/92fecef5-9e67-448e-b436-6c91fc3af9fc/-TRgaPFVEaga/og_1741427114_icons8-instagram1.svg",
+              fileOriginalName: "icons8-instagram (1).svg",
+              alternateFilePath: null,
+              alternateThumbnail: null,
+            },
+          ],
+        },
+        {
+          id: "d51c2231-1393-48d8-a5af-bcdd64b29e81",
+          url: "https://www.facebook.com/shapesproducts",
+          icons: [
+            {
+              src: "https://assets.cms.ecollat.com/cmsassets/null/92fecef5-9e67-448e-b436-6c91fc3af9fc/rhrvTAj-XKUb/og_1741341469_icons8-facebook-f.svg",
+              caption: null,
+              filePath:
+                "https://assets.cms.ecollat.com/cmsassets/null/92fecef5-9e67-448e-b436-6c91fc3af9fc/rhrvTAj-XKUb/og_1741341469_icons8-facebook-f.svg",
+              fileSize: "499 bytes",
+              dimension: null,
+              mediaType: "image",
+              mediaFileId: "rhrvTAj-XKUb",
+              alternateName: null,
+              thumbnailPath:
+                "https://assets.cms.ecollat.com/cmsassets/null/92fecef5-9e67-448e-b436-6c91fc3af9fc/rhrvTAj-XKUb/og_1741341469_icons8-facebook-f.svg",
+              fileOriginalName: "icons8-facebook-f.svg",
+              alternateFilePath: null,
+              alternateThumbnail: null,
+            },
+          ],
+          title: "facebook",
+          whiteicon: [
+            {
+              src: "https://assets.cms.ecollat.com/cmsassets/null/92fecef5-9e67-448e-b436-6c91fc3af9fc/zcJ5lQQPZyQZ/og_1741427126_icons8-facebook-f1.svg",
+              caption: null,
+              filePath:
+                "https://assets.cms.ecollat.com/cmsassets/null/92fecef5-9e67-448e-b436-6c91fc3af9fc/zcJ5lQQPZyQZ/og_1741427126_icons8-facebook-f1.svg",
+              fileSize: "700 bytes",
+              dimension: null,
+              mediaType: "image",
+              mediaFileId: "zcJ5lQQPZyQZ",
+              alternateName: null,
+              thumbnailPath:
+                "https://assets.cms.ecollat.com/cmsassets/null/92fecef5-9e67-448e-b436-6c91fc3af9fc/zcJ5lQQPZyQZ/og_1741427126_icons8-facebook-f1.svg",
+              fileOriginalName: "icons8-facebook-f (1).svg",
+              alternateFilePath: null,
+              alternateThumbnail: null,
+            },
+          ],
+        },
+      ],
+    },
+  };
 
   return (
     <div id="footer" className="footer bg-surface">
